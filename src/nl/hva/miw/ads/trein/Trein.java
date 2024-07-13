8811cd2c36b34460d8c93f9831c4a54ed50dfa67
Danny Kwant <danny.kwant@hva.nl>
2024-07-13 15:59:48 +0200

    feat(model): implements verwijderWagon, geefWagonNr, voegWagonToe en geefPositie in Trein

--- src/nl/hva/miw/ads/trein/Trein.java
+++ src/nl/hva/miw/ads/trein/Trein.java
@@ -4,15 +4,12 @@ package nl.hva.miw.ads.trein;
 /**
  * Trein klasse. Een Trein is een keten van wagons.
  * Elke wagon heeft een nr.
- *
+ * <p>
  * Wagons hebben een positie in de Trein. De posities beginnen bij 1 (niet 0!)
- *
  */
 public class Trein {
     private int lengte;
-
     private Wagon kop;
-
 
     public Trein() {
         this.lengte = 0;
@@ -25,17 +22,28 @@ public class Trein {
 
     /**
      * Geeft de wagonnr van de wagon op gegeven positie.
-     *
+     * <p>
      * Als er geen wagon op die positie niet bestaat wordt -1 teruggegeven.
-     *
+     * <p>
      * Let op: de eerste wagon zit op positie 1.
      *
      * @param positie
      * @return
      */
     public int geefWagonNr(int positie) {
-        // Implementeer deze methode
-        return -1;
+        if (positie < 1 || positie > this.lengte) return -1;
+
+        Wagon huidigeWagon = this.kop;
+        for (int i = 1; i < positie; i++) {
+            if (huidigeWagon == null) return -1;
+            huidigeWagon = huidigeWagon.getVolgende();
+        }
+
+        if (huidigeWagon != null) {
+            return huidigeWagon.getNr();
+        } else {
+            return -1;
+        }
     }
 
     /**
@@ -45,11 +53,20 @@ public class Trein {
      * @param wagonNr
      * @return
      */
-    public int geefPositie( int wagonNr ) {
-        // Implementeer deze methode
+    public int geefPositie(int wagonNr) {
+        Wagon huidigeWagon = this.kop;
+        int positie = 1;
 
+        while (huidigeWagon != null) {
+            if (huidigeWagon.getNr() == wagonNr) {
+                return positie;
+            }
+            huidigeWagon = huidigeWagon.getVolgende();
+            positie++;
+        }
         return 0;
     }
+
 
     /**
      * Voegt een wagon met wagonNr toe aan de Trein op de gegeven positie.
@@ -58,11 +75,70 @@ public class Trein {
      *
      * @param wagonNr - wagon nummer.
      */
-    public void voegWagonToe( int wagonNr, int positie ) {
-//        System.out.println("voegWagonToe: " + wagonNr + " op positie " + positie );
+    public void voegWagonToe(int wagonNr, int positie) {
+        Wagon nieuweWagon = new Wagon(wagonNr);
 
-        // Implementeer deze methode.
+        if (positie <= 1) {
+            voegWagonAanKopToe(nieuweWagon);
+        } else {
+            voegWagonOpPositieToe(nieuweWagon, positie);
+        }
+        this.lengte++;
+    }
 
+    /**
+     * hulpmethode voor voegWagonToe
+     *
+     * @param nieuweWagon
+     */
+    private void voegWagonAanKopToe(Wagon nieuweWagon) {
+        nieuweWagon.setVolgende(this.kop);
+
+        if (this.kop != null) {
+            this.kop.setVorige(nieuweWagon);
+        }
+
+        this.kop = nieuweWagon;
+    }
+
+    /**
+     * hulpmethode voor voegWagonToe
+     *
+     * @param nieuweWagon
+     * @param positie
+     */
+    private void voegWagonOpPositieToe(Wagon nieuweWagon, int positie) {
+        Wagon huidigeWagon = this.kop;
+        for (int i = 1; i < positie - 1 && huidigeWagon != null; i++) {
+            huidigeWagon = huidigeWagon.getVolgende();
+        }
+
+        if (huidigeWagon != null) {
+            nieuweWagon.setVolgende(huidigeWagon.getVolgende());
+            if (huidigeWagon.getVolgende() != null) {
+                huidigeWagon.getVolgende().setVorige(nieuweWagon);
+            }
+            huidigeWagon.setVolgende(nieuweWagon);
+            nieuweWagon.setVorige(huidigeWagon);
+        } else {
+            voegWagonAanEindeToe(nieuweWagon);
+        }
+    }
+
+    /**
+     * hulpmethode voor voegWagonToe
+     *
+     * @param nieuweWagon
+     */
+    private void voegWagonAanEindeToe(Wagon nieuweWagon) {
+        Wagon huidigeWagon = this.kop;
+
+        while (huidigeWagon.getVolgende() != null) {
+            huidigeWagon = huidigeWagon.getVolgende();
+        }
+
+        huidigeWagon.setVolgende(nieuweWagon);
+        nieuweWagon.setVorige(huidigeWagon);
     }
 
     /**
@@ -71,11 +147,49 @@ public class Trein {
      *
      * @param positie - positie.
      */
-    public void verwijderWagon( int positie ) {
-//        System.out.println("verwijderWagon op positie " + positie );
+    public void verwijderWagon(int positie) {
+        if (positie < 1 || positie > this.lengte) return;
 
-        // Implementeer deze methode.
+        if (positie == 1) {
+            verwijderEersteWagon();
+        }
+        else {
+            verwijderWagonOpPositie(positie);
+        }
+        this.lengte--;
+    }
 
+    /**
+     * hulpmethode voor verwijderWagon
+     */
+    private void verwijderEersteWagon() {
+        if (this.kop != null) {
+            this.kop = this.kop.getVolgende();
+            if (this.kop != null) {
+                this.kop.setVorige(null);
+            }
+        }
+    }
+
+    /**
+     * hulpmethode voor verwijderWagon
+     *
+     * @param positie
+     */
+    private void verwijderWagonOpPositie(int positie) {
+        Wagon huidigeWagon = this.kop;
+        for (int i = 1; i < positie; i++) huidigeWagon = huidigeWagon.getVolgende();
+
+        if (huidigeWagon != null) {
+            Wagon vorigeWagon = huidigeWagon.getVorige();
+            Wagon volgendeWagon = huidigeWagon.getVolgende();
+            if (vorigeWagon != null) {
+                vorigeWagon.setVolgende(volgendeWagon);
+            }
+            if (volgendeWagon != null) {
+                volgendeWagon.setVorige(vorigeWagon);
+            }
+        }
     }
 
     /**
@@ -103,8 +217,8 @@ public class Trein {
         StringBuffer sb = new StringBuffer();
         sb.append("Trein{ lengte=" + lengte + " }");
         Wagon w = kop;
-        while (w != null ) {
-            sb.append( " " + w.getNr() );
+        while (w != null) {
+            sb.append(" " + w.getNr());
             w = geefVolgende(w);
         }
 
@@ -113,6 +227,7 @@ public class Trein {
 
     /**
      * Geef de trein in omgekeerde volgorde.
+     *
      * @return
      */
     public String omgekeerdeTrein() {
@@ -120,15 +235,16 @@ public class Trein {
         sb.append("Trein{ lengte=" + lengte + " }");
         Wagon w = kop;
         // Loop naar de laatste wagon
-        while (w != null && geefVolgende(w) != null ) {
+        while (w != null && geefVolgende(w) != null) {
             w = geefVolgende(w);
         }
 
-        while (w != null ) {
-            sb.append( " " + w.getNr() );
+        while (w != null) {
+            sb.append(" " + w.getNr());
             w = geefVorige(w);
         }
 
         return sb.toString();
     }
+
 }
